f73ba5cf52981a46d1d0d0f141e7297961027836
kirill <golovinkiruxa@gmail.com>
2024-03-28 19:37:09 +0100

    Add project files.

---
+++ HelloWorlds.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+// call function std::cout
+void print()
+{
+    //call operator << on obj std::cout
+    std::cout << "Hello\n"; //print
+}
+/*
+ctrl+k+c - comment
+ctrl+k+u - uncomment
+*/
+int main()
+{
+    int x = 100;
+    3 + 7;
+    int y = x + 100;
+
+    int b = 0;
+    b = b + 2;
+
+    int test;
+    test = 100500;
+
+    int test2 = 1005001;
+
+    int mult = x * y;
+    int random = 1002;
+
+    std::cout << "Hello World!\n";
+
+}
